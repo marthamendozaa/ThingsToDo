76ff88011099f5ef1cc17aa4eaec8d10e28f4c78
Martha Mendoza <mmarthamendozaa@gmail.com>
2024-12-10 14:29:42 +0100

    creation

---
+++ ThingsToDo/ThingsToDo/ThingsToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsToDoApp.swift
+//  ThingsToDo
+//
+//  Created by Martha Mendoza Alfaro on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ThingsToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
